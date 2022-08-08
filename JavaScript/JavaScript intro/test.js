d6213f308e18be75dde10298de9e0695abcb2e5c
Lynx <75997156+Lynxxu@users.noreply.github.com>
2022-08-07 22:54:26 -0400

    Started numbers and operators

---
+++ JavaScript/JavaScript intro/test.js
@@ -0,0 +1,12 @@
+// numbers are the same type
+const myNumber = 1;
+const anotherNumber = 2.3;
+console.log(typeof myNumber);
+console.log(typeof anotherNumber);
+
+//methods
+const manyDecimal = 1.23432546547475;
+console.log(manyDecimal);
+
+const twoDecimal = manyDecimal.toFixed(2);
+console.log(twoDecimal);
