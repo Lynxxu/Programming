9fc199e93d518afe0ee554546f2ada5e6c23d5b4
Lynx <75997156+Lynxxu@users.noreply.github.com>
2022-08-09 17:27:38 -0400

    finished string

--- JavaScript/JavaScript intro/test.js
+++ JavaScript/JavaScript intro/test.js
@@ -15,3 +15,26 @@ console.log(twoDecimal);
 let num = 1;
 console.log(num++);
 console.log(num);
+
+const hisNum = num;
+console.log(hisNum);
+
+const singleQuote = 'this is a "single" quote';
+const doubleQuote = "this is a double's quote";
+console.log(singleQuote);
+console.log(doubleQuote);
+
+name = "chalin";
+const greeting = `hello, ${name}`;
+console.log(greeting);
+
+const surname = "Yang";
+const lastName = "chalin";
+const literal = `${surname}${lastName}`;
+console.log(literal);
+
+const candidate = "Chalin";
+const score = 9;
+const maxScore = 10;
+const output = `Hi ${candidate}, Your score is ${(score / maxScore) * 100}%.`;
+console.log(output);
