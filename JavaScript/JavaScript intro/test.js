3fde1c756e7fea3601fed3294503b02317dcb881
Lynx <75997156+Lynxxu@users.noreply.github.com>
2022-08-10 15:57:57 -0400

    Strign methods finished

--- JavaScript/JavaScript intro/test.js
+++ JavaScript/JavaScript intro/test.js
@@ -24,7 +24,7 @@ const doubleQuote = "this is a double's quote";
 console.log(singleQuote);
 console.log(doubleQuote);
 
-name = "chalin";
+const name = "chalin";
 const greeting = `hello, ${name}`;
 console.log(greeting);
 
@@ -38,3 +38,26 @@ const score = 9;
 const maxScore = 10;
 const output = `Hi ${candidate}, Your score is ${(score / maxScore) * 100}%.`;
 console.log(output);
+
+const browserType = "chrome";
+console.log(browserType.length);
+
+const bigString = "chalin jim and oscar are groups of idiots idiots";
+const firstOccurrence = bigString.indexOf("idiots");
+const secondOccurrence = bigString.indexOf("idiots", firstOccurrence + 1);
+
+console.log(firstOccurrence);
+console.log(secondOccurrence);
+
+console.log(browserType.slice(1, 2));
+console.log(browserType.slice(1));
+
+const messedCap = "SOme RandOM caPtaliZed WorDs";
+console.log(messedCap.toLowerCase());
+console.log(messedCap.toUpperCase());
+
+let sample = "chalin is messing up with jim's network with oscar";
+const updatedSample = sample.replace("jim", "william");
+
+console.log(sample);
+console.log(updatedSample);
