425b63fb746b1ce96111b460db3c0dfb82ab80a2
Lynx <75997156+Lynxxu@users.noreply.github.com>
2023-02-24 23:40:03 -0500

    Finished learning components

---
+++ React/Docs/Describing UI/components/components.js
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+function Profile() {
+  return <img src="../../../../Imgs/83181495_p0.jpg" alt="Katherine Johnson" />;
+}
+
+let App = function Gallery() {
+  return (
+    <section>
+      <h1>Amazing fuhua</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
