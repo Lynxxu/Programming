d13147f453e7af3d00354beb21fc44d6dc4f132e
Lynx <75997156+Lynxxu@users.noreply.github.com>
2023-03-05 23:17:19 -0500

    Finsihed learning Render

--- React/Docs/Describing UI/Rendering/Rendering.js
+++ React/Docs/Describing UI/Rendering/Rendering.js
@@ -43,3 +43,59 @@ function PackingListUpdate() {
 
 const crossed = createRoot(document.getElementById("crossed"));
 crossed.render(<PackingListUpdate />);
+
+const people = [
+  "Jim: Biochemistry",
+  "WIll: Chemistry",
+  "Chalin: Math and Stats",
+  "Oscar: cog science",
+];
+
+function List1() {
+  const listItem = people.map((person) => <li>{person}</li>);
+  return <ul>{listItem}</ul>;
+}
+
+const list1 = createRoot(document.getElementById("list1"));
+list1.render(<List1 />);
+
+const people2 = [
+  {
+    id: 0,
+    name: "Jim",
+    major: "BioChem",
+  },
+  {
+    id: 1,
+    name: "Will",
+    major: "Chem",
+  },
+  {
+    id: 2,
+    name: "Chalin",
+    major: "math and stats",
+  },
+  {
+    name: "Oscar",
+    major: "cog sci",
+  },
+  {
+    name: "KK",
+    major: "Chem",
+  },
+];
+
+const chemists = people2.filter((person) => person.major === "Chem");
+
+function List2() {
+  const listItem2 = chemists.map((person) => <li>Name: {person.name}</li>);
+  return (
+    <>
+      <h3>List of Chem major students</h3>
+      <ul>{listItem2}</ul>
+    </>
+  );
+}
+
+const list2 = createRoot(document.getElementById("list2"));
+list2.render(<List2 />);
