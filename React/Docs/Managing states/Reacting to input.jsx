ed51bf9d312ecd67a94c47a265e6b3c554af4090
Lynx <75997156+Lynxxu@users.noreply.github.com>
2023-05-11 10:42:47 +0800

    finished reacintg to input

--- React/Docs/Managing states/Reacting to input.jsx
+++ React/Docs/Managing states/Reacting to input.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 function Form1(status = "empty") {
@@ -42,3 +42,63 @@ export function Form2(status = "empty") {
     </>
   );
 }
+
+function FormFinal() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+
+  if (status === "success") {
+    return <h1>Cograts, you are right</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (Error) {
+      setStatus("typing");
+      setError(Error);
+    }
+  }
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Genshin pop quiz</h2>
+      <p>What's the surname of Xianglin?</p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === "submitting"}>
+          Submit
+        </button>
+        {error !== null && <p className="Error">{error.message}</p>}
+      </form>
+    </>
+  );
+}
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== "mao";
+      if (shouldError) {
+        reject(new Error("Nope, thats wrong, try again"));
+      } else {
+        resolve();
+      }
+    }, 150);
+  });
+}
+
+const formfinal = createRoot(document.getElementById("Formfinal"));
+formfinal.render(<FormFinal />);
